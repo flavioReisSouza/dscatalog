532c86330350e64c298de65fb23a77df96cd161f
Flavio Reis <ext.flavios@unicid.edu.br>
2024-02-06 11:33:28 -0300

    CategoryController

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscatalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+  private Long id;
+  private String name;
+}
