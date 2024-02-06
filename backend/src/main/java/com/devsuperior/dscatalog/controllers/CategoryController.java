8fea610be1e598092664e146a0f5ac4bd3edc56f
Flavio Reis <ext.flavios@unicid.edu.br>
2024-02-06 20:53:55 -0300

    Post, Put, Delete and DatabaseException

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -2,13 +2,19 @@ package com.devsuperior.dscatalog.controllers;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
+import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +23,7 @@ public class CategoryController {
 
   private final CategoryService service;
 
-  @GetMapping()
+  @GetMapping
   public ResponseEntity<List<CategoryDTO>> findAll() {
     List<CategoryDTO> list = service.findAll();
     return ResponseEntity.ok(list);
@@ -27,5 +33,28 @@ public class CategoryController {
   public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
     CategoryDTO dto = service.findById(id);
     return ResponseEntity.ok(dto);
+  }
+
+  @PostMapping
+  public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO dto) {
+    dto = service.create(dto);
+    URI uri =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(dto.getId())
+            .toUri();
+    return ResponseEntity.created(uri).body(dto);
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+    dto = service.update(id, dto);
+    return ResponseEntity.ok(dto);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
   }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,11 +3,15 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -27,5 +31,37 @@ public class CategoryService {
     Optional<Category> obj = repository.findById(id);
     Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
     return new CategoryDTO(entity);
+  }
+
+  @Transactional
+  public CategoryDTO create(CategoryDTO dto) {
+    Category entity = new Category();
+    entity.setName(dto.getName());
+    entity = repository.save(entity);
+    return new CategoryDTO(entity);
+  }
+
+  @Transactional
+  public CategoryDTO update(Long id, CategoryDTO dto) {
+    try {
+      Category entity = repository.getReferenceById(id);
+      entity.setName(dto.getName());
+      entity = repository.save(entity);
+      return new CategoryDTO(entity);
+    } catch (EntityNotFoundException e) {
+      throw new ResourceNotFoundException("Id not found " + id);
+    }
+  }
+
+  @Transactional(propagation = Propagation.SUPPORTS)
+  public void delete(Long id) {
+    if (!repository.existsById(id)) {
+      throw new ResourceNotFoundException("Id not found " + id);
+    }
+    try {
+      repository.deleteById(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException("Referenced integrity failure");
+    }
   }
 }
